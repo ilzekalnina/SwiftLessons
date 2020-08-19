ed20565a8df89de49815ca0a56c0efff48359e21
Ilze Kalnina <ilze@skate.lv>
2020-08-19 19:12:40 +0300

    made changes in Lesson10

--- Lesson10-Xcode.playground/Contents.swift
+++ Lesson10-Xcode.playground/Contents.swift
@@ -1,3 +1,10 @@
 import UIKit
 // Lesson 10
 //trying commit
+
+// Go to source control - commit
+
+//chek in the left coner push, then commit
+// my token 3b3525e77b0c5954b335673cb74b54fd97e6c3cd
+
+
